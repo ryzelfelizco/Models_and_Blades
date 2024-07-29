59a4f8f72f72743b696a8242c41d644d3bb5dacf
Ryzel <ryzelerinfelizco@gmail.com>
2024-07-30 06:16:58 +0800

    Create book factory

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -2,22 +2,22 @@
 
 namespace Database\Factories;
 
+use App\Models\Book;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
- */
 class BookFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Book::class;
+
+    public function definition()
     {
         return [
-            //
+            'isbn' => $this->faker->isbn13(),
+            'title' => $this->faker->sentence(),
+            'author' => $this->faker->name(),
+            'description' => $this->faker->paragraph(),
+            'date_published' => $this->faker->date(),
         ];
     }
 }
