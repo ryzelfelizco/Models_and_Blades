9d8ef6fe22494178a2d9fa36c6b67c206542dcbc
Ryzel <ryzelerinfelizco@gmail.com>
2024-07-30 06:12:20 +0800

    Updated Book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    // Define fillable fields if necessary
+    protected $fillable = ['isbn', 'title', 'author', 'description', 'date_published'];
 }
