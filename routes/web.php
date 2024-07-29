2db344ab32af4816a443f89f385edc0cd252eeaa
Ryzel <ryzelerinfelizco@gmail.com>
2024-07-30 06:23:14 +0800

    Modified controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/books', [BookController::class, 'index']);
